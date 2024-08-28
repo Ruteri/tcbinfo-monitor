e1c7e3e9c9d9036d7e8617f413aaad231f2f48a9
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2024-08-28 19:28:18 +0200

    Also fetch TDX TCBs

--- fetch_tcbs.py
+++ fetch_tcbs.py
@@ -4,10 +4,18 @@ import sys
 
 res = requests.get('https://api.trustedservices.intel.com/sgx/certification/v4/fmspcs')
 
-all_data = {}
+all_data = []
 all_fmpcs = json.loads(res.text)
 for spec in all_fmpcs:
     res = requests.get('https://api.trustedservices.intel.com/sgx/certification/v4/tcb?fmspc='+spec['fmspc']+'&update=early')
-    all_data[spec['fmspc']] = json.loads(res.text)
+    if res.ok == False:
+        sys.exit(1)
+    all_data.append(res.json())
+
+    res = requests.get('https://api.trustedservices.intel.com/tdx/certification/v4/tcb?fmspc='+spec['fmspc']+'&update=early')
+    if res.status_code == 200:
+        all_data.append(res.json())
+    elif res.status_code != 404:
+        sys.exit(1)
 
 print(json.dumps(all_data, indent=2))
